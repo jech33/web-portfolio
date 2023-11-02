1b283f112ef784aee2d36730589b2f2b4a5f8750
Javier Echavez <jechavezch@hotmail.com>
2023-11-02 07:29:44 -0500

    FIX(WorkCard): change conditional render logic isLoadingImage

--- app/_components/WorkCard.tsx
+++ app/_components/WorkCard.tsx
@@ -15,7 +15,7 @@ const WorkCard = (props: Work) => {
   return (
     <article className="inline-block max-w-[25rem] gap-2">
       <section className="relative flex flex-col gap-2">
-        {!isLoadingImage && (
+        {isLoadingImage && (
           <div className="after:content-[' '] absolute top-0 z-[1] h-[300px] w-full bg-background-dark after:absolute after:h-[300px] after:w-full after:animate-pulse after:bg-background-light md:h-[450px] md:after:h-[450px]" />
         )}
         <Image
